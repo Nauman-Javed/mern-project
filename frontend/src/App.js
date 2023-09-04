2645e76b0eeb4976cf1403f76f07cd80dc0c87ee
Nauman-Javed <naumanj841@gmail.com>
2023-09-04 12:27:25 +0500

    configure redux store and created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard, Login, Register } from "./pages";
+import { Header } from "./components";
 function App() {
-  return <div>React App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
